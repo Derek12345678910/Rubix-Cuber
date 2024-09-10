7fca8af55dc1c1d9614acb469537aa66745e59a6
Your Namedwadwadadfwadwadhawhjduwahdgit config --global user.email you@example.comgit config --global user.name Your <drohi2026@gmail.com>
2024-09-09 20:20:25 -0400

    finished getting cube information

--- backend/cube.h
+++ backend/cube.h
@@ -2,10 +2,77 @@
 #define cube_h
 
 // standard libraries
-
+#include <map>
 
 // standard library
 using namespace std;
+
+// create a structure of the cube
+struct Cube{
+    // Side, Colour
+    map<char, char> cubeFaces; // Tells us which colour is which part of a fixed cube
+    /* Numbering of face sides goes from
+    Top --> 0 --> T 
+    Bottom --> 1 --> O
+    Back --> 2 --> B
+    Front --> 3 -- F
+    Left --> 4 --> L
+    Right --> 5 --> R
+    */
+    char faceValues[6] = {'T', 'O', 'B', 'F', 'L', 'R'}; // array of side types in order
+
+    /* Corner Positions --> the corner number and the corresponding sides they are located on
+    0 --> Top, Back, Left
+    1 --> Top, Back, Right
+    2 --> Top, Front, Left
+    3 --> Top, Front, Right
+    4 --> Bottom, Back, Left
+    5 --> Bottom, Back, Right
+    6 --> Bottom, Front, Left
+    7 --> Bottom, Front, Right
+    */
+   int cornerValues[4] = {0, 2, 6, 8}; // array of all corner pieces on a face
+   map<int, vector<char>> cornerPos = {
+    {0, {'T', 'B', 'L'}},  // 0: Top, Back, Left
+    {1, {'T', 'B', 'R'}},  // 1: Top, Back, Right
+    {2, {'T', 'F', 'L'}},  // 2: Top, Front, Left
+    {3, {'T', 'F', 'R'}},  // 3: Top, Front, Right
+    {4, {'B', 'B', 'L'}},  // 4: Bottom, Back, Left
+    {5, {'B', 'B', 'R'}},  // 5: Bottom, Back, Right
+    {6, {'B', 'F', 'L'}},  // 6: Bottom, Front, Left
+    {7, {'B', 'F', 'R'}}   // 7: Bottom, Front, Right
+    };
+
+    /* Edge positions --> the edge number and the corresponding sides they are located on
+    0 --> Top, Front
+    1 --> Top, Right
+    2 --> Top, Back
+    3 --> Top, Left
+    4 --> Bottom, Front
+    5 --> Bottom, Right
+    6 --> Bottom, Back
+    7 --> Bottom, Left
+    8 --> Front, Right
+    9 --> Front, Left
+    10 --> Back, Right
+    11 --> Back, Left
+    */
+   int edgeValues[4] = {1, 3, 5, 7}; // array of all edge pieces on a face
+   map<int, vector<char>> edgePos = {
+    {0, {'T', 'F'}},  // 0: Top, Front
+    {1, {'T', 'R'}},  // 1: Top, Right
+    {2, {'T', 'B'}},  // 2: Top, Back
+    {3, {'T', 'L'}},  // 3: Top, Left
+    {4, {'B', 'F'}},  // 4: Bottom, Front
+    {5, {'B', 'R'}},  // 5: Bottom, Right
+    {6, {'B', 'B'}},  // 6: Bottom, Back
+    {7, {'B', 'L'}},  // 7: Bottom, Left
+    {8, {'F', 'R'}},  // 8: Front, Right
+    {9, {'F', 'L'}},  // 9: Front, Left
+    {10, {'B', 'R'}}, // 10: Back, Right
+    {11, {'B', 'L'}}  // 11: Back, Left
+    };
+};
 
 // creates a structure of the cubes edges
 struct Edge {
@@ -27,10 +94,12 @@ class cube
 public:
 
     // raw cube information
-    char* faces[6][3][3];
+    char* facesInput[6][3][3];
 
     // solving cube information
-    
+    Corner* corners[8]; // holds all corners
+    Edge* edges[12]; // holds all edges
+    Cube* cuber = nullptr; // holds the current cube we are working on
 
     // constructor
     cube(const char* information); // points to information given
--- backend/src/cube.cpp
+++ backend/src/cube.cpp
@@ -1,14 +1,22 @@
 #include "../main.h"
 #include "../cube.h"
 
-// Controls the cube itself
-
 // Function to get row and column from a linear index
 pair<int, int> squareReturn(int index)
 {
     int row = index / 3;
     int col = index % 3;
     return make_pair(row, col);
+}
+
+// Function find the key using the value // ONLY FOR CHAR
+int findKey(const map<char, char> m, char value) {
+    for (const auto& pair : m) {
+        if (pair.second ==  value) {
+            return pair.first;  // Return the key if the value matches
+        }
+    }
+    return -1;  // Return -1 if the value is not found
 }
 
 // Constructor
@@ -19,33 +27,136 @@ cube::cube(const char* information){
     // loop through the faces and grab information from each face
     for (int face = 0; face < 6; ++face)
     {
+        //cout << '\n' << face << ": ";
         for (int i = 0; i < 9; ++i)  // 9 squares per face
         {
             auto [row, col] = squareReturn(i);  // Get row and col from the index
-            faces[face][row][col] = new char; // Allocate memory
-            *faces[face][row][col] = information[index++];  // Assign character
+            facesInput[face][row][col] = new char; // Allocate memory
+            *facesInput[face][row][col] = information[index++];  // Assign character
+            //cout << row << " " << col << " " << facesInput[face][row][col] << " ";
         }
-    }
+    } // COMPLETE
 
-    // Using the information create edges and corners to store
+    // create cube object
+    Cube* cubeObj = new Cube;
+    cuber = cubeObj;
+    // distinguish top - bottom - front - back
+    for(int face = 0; face < 6; ++face){ // find the middle of each face and assign each to a direction
+        //cout << "\n" << face << ": ";
+        auto[row, col] = squareReturn(4); // return the middle of the face --> 5-1 because its center / arry
+        cuber->cubeFaces[cuber->faceValues[face]] = *facesInput[face][row][col]; // Set cubeFace map
+        //auto test = cuber->cubeFaces.find(cuber->faceValues[face]);
+        //cout << test->first << " " << test->second << " ";
+    } // COMPLETE
 
-    
+    // Create 8 corners 
+    for(int i = 0; i < 8; ++i){
+        Corner* corner = new Corner; // create corner object
+        corner->position = i; // set position
+
+        auto[row, col] = squareReturn(cuber->cornerValues[i % 4]); // return corner values as [row, col]
+        auto[mid1, mid2] = squareReturn(4); // middle of square
+        int colIndex = 0; // used for the colour // is just the index of d
+        // we do not know which face the corner is, so we must compare the faces it should have with the actual faces, and when we do we know which
+        for(auto d : cuber->cornerPos[i]){ // loop the dimensions for the certain corner
+            for(int face = 0; face < 6; ++face){ // loop all faces on the cube
+                if(*facesInput[face][mid1][mid2] == cuber->cubeFaces[d]){ // if the dimension matches the face, WE ARE ON THE CORRECT FACE
+                    corner->colors[colIndex] = *facesInput[face][row][col]; // set the colour
+                    // Orientation
+                    auto cornerCol = cuber->cubeFaces.find(findKey(cuber->cubeFaces, corner->colors[colIndex])); // find the side the face is supposed to be on
+                    auto comparedCol = cuber->cubeFaces.find(findKey(cuber->cubeFaces, *facesInput[face][mid1][mid2]));
+                    if(cornerCol->first == 'T' || cornerCol->first == 'O'){ // Only use top or bottom
+                        // if the corner pieces top - bottom is facing front - back the orientation --> 1
+                        if(comparedCol->first == 'F' || comparedCol->first == 'B'){
+                            corner->orientation = 1;
+                        }
+                        // if the corner pieces top - bottom is NOT facing front - back the orientation is --> 2
+                        if(comparedCol->first == 'L' || comparedCol->first == 'R'){
+                            corner->orientation = 2;
+                        }
+                        // if piece is perfect orientation is --> 0
+                        if(comparedCol->first == 'T' || comparedCol->first == 'O'){
+                            corner->orientation = 0;
+                        }
+                    } 
+                }
+            } 
+            colIndex ++;        
+        }
+        // Assign corner to an array of corner holders
+        corners[i] = corner;
+        /*
+        cout << "Position: " << corner->position << endl;
+        cout << "Orientation: " << corner->orientation << endl;
+        cout << "Colour 1: " << corner->colors[0] << " Colour 2: " << corner->colors[1] << " Colour 3: " << corner->colors[2] << endl;
+        */
+    } // COMPLETE
+
+    // Create 12 edges
+    for(int i = 0; i < 12; ++i){
+        Edge* edge = new Edge; // create edge object
+        edge->position = i; // set position
+        auto[row, col] = squareReturn(cuber->edgeValues[i % 4]); // return corner values as [row, col]
+        auto[mid1, mid2] = squareReturn(4); // middle of square
+        int colIndex = 0; // used for the colour // is just the index of d
+        // we do not know which face the edge is, so we must compare the faces it should have with the actual faces, and when we do we know which
+        for(auto d : cuber->edgePos[i]){ // loop the dimensions for the certain corner
+            for(int face = 0; face < 6; ++face){ // loop all faces on the cube
+                if(*facesInput[face][mid1][mid2] == cuber->cubeFaces[d]){ // if the dimension matches the face, WE ARE ON THE CORRECT FACE
+                    edge->colors[colIndex] = *facesInput[face][row][col]; // set the colour
+                     // Orientation
+                    auto cornerCol = cuber->cubeFaces.find(findKey(cuber->cubeFaces, edge->colors[colIndex])); // find the side the face is supposed to be on
+                    auto comparedCol = cuber->cubeFaces.find(findKey(cuber->cubeFaces, *facesInput[face][mid1][mid2]));
+                    if(cornerCol->first == 'T' || cornerCol->first == 'O'){ // Only use top or bottom
+                        // if the top - bottom piece is not facing top - bottom orientation --> 1
+                        if(comparedCol->first == 'F' || comparedCol->first == 'B'){
+                            edge->orientation = 1;
+                        }
+                        // if piece's top - bottom is facing top - bottom orientation --> 0
+                        if(comparedCol->first == 'L' || comparedCol->first == 'R'){
+                            edge->orientation = 2;
+                        }
+                    }
+                    
+                }
+            }
+            colIndex ++;
+        }
+        edges[i] = edge;
+        /*
+        cout << "Position: " << edge->position << endl;
+        cout << "Orientation: " << edge->orientation << endl;
+        cout << "Colour 1: " << edge->colors[0] << " Colour 2: " << edge->colors[1] << endl;
+        */
+    }   
 }
 
 // Destructor
 cube::~cube(){
-    // Destroy cube information
-    int index = 0; // To track the index in the 1D information array
 
-    // loop through the faces and grab information from each face
-    for (int face = 0; face < 6; ++face)
+    // destroy face information
+    for (int face = 0; face < 6; ++face) // loop through the faces and grab information from each face
     {
         for (int i = 0; i < 9; ++i)  // 9 squares per face
         {
             auto [row, col] = squareReturn(i);  // Get row and col from the index
-            delete faces[face][row][col]; // deletes the value one by one
+            delete facesInput[face][row][col]; // deletes the value one by one
         }
     }
+
+    // Delete corner pointers
+    for (int i = 0; i < 8; ++i) {
+        delete corners[i];  // Delete each corner pointer
+    }
+
+    // Delete edge pointers
+    for (int i = 0; i < 12; ++i) {
+        delete edges[i];  // Delete each edge pointer
+    }
+
+    // delete cube object
+    delete cuber; 
+
 }
 
 
